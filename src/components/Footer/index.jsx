4075a1d7d3770ae3eda38f7d740296ea8324763d
alex <lapkesa@gmail.com>
2023-04-24 01:50:36 +0300

    feat:add footer section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import style from './Footer.module.css'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className={style.footer}>
+      <div className={style.footerContainer}>
+        <h3 className={style.heading}>Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+        <div className={style.info}>
+          <p className={style.year}>&#169; 2023</p>
+          <nav className={style.links}>
+            <a
+              href='https://practicum.yandex.ru/'
+              className={style.link}>
+              Яндекс.Практикум
+            </a>
+            <a
+              href='https://github.com/lu1dgy'
+              className={style.link}>
+              Github
+            </a>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
