b73a438d0b3050f729d75f38261d47df2c7e7b49
alex <lapkesa@gmail.com>
2023-04-24 01:25:50 +0300

    feat:add porfolio section

--- src/components/Main/Portfolio/index.jsx
+++ src/components/Main/Portfolio/index.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import style from './Portfolio.module.css'
+import arrow from '../../../images/arrow.svg'
 
 const Portfolio = () => {
-  return <div>Portfolio</div>
+  const links = [
+    { link: 'Статичный сайт', href: '#' },
+    { link: 'Адаптивный сайт', href: '#' },
+    { link: 'Одностраничное приложение', href: '#' },
+  ]
+
+  return (
+    <section className={style.portfolio}>
+      <div className={style.portfolioContainer}>
+        <h2 className={style.heading}>Портфолио</h2>
+        <ul className={style.list}>
+          {links.map((e, i) => (
+            <li
+              className={style.item}
+              key={i}>
+              <a
+                className={style.link}
+                href={e.href}>
+                <span className={style.text}>{e.link}</span>
+                <img
+                  className={style.arrow}
+                  src={arrow}
+                  alt='arrow'
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Portfolio
