8db168fc7be6c2893964342d0dc0184c79f7b183
alex <lapkesa@gmail.com>
2023-05-10 21:05:54 +0300

    feat: add getMovies func

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { CurrentUserContext } from './context/CurrentUserContext';
 import Login from './pages/Login';
@@ -8,22 +9,35 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
 import SavedMovies from './pages/SavedMovies';
+import { moviesApi } from './utils/MoviesApi';
 
 const App = () => {
+  const [loading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = () => {
+    setIsLoading(true);
+    moviesApi
+      .getMovies()
+      .then((data) => setMovies(data))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   return (
     <>
-    <CurrentUserContext.Provider>
-    <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/saved-movies' element={<SavedMovies />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </CurrentUserContext.Provider>
+      <CurrentUserContext.Provider>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/saved-movies' element={<SavedMovies />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </CurrentUserContext.Provider>
     </>
   );
 };
