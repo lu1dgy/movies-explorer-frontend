c67ba7af06bbe61e2d82a7c0e3b42b7dc29bda58
alex <lapkesa@gmail.com>
2023-05-18 21:52:55 +0300

    change to prod move

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -98,8 +98,7 @@ class MainApi {
 }
 
 const mainApi = new MainApi({
-  // url: 'https://api.movies.lapkes.nomoredomains.monster',
-  url: 'http://localhost:3100',
+  url: 'https://api.movies.lapkes.nomoredomains.monster',
   headers: {
     'Content-Type': 'application/json',
   },
